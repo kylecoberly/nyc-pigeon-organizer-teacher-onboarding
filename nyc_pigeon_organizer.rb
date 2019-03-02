47b3888732559a007fdf39ddf8236ecd1d521645
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-02 01:11:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,8 +13,8 @@ def nyc_pigeon_organizer(data)
 end
 
 def find_matches data, key, name
-  data[key].reduce([]) {|matching_people, (color_label, people)|
-    matching_people << name if people.include? name
+  data[key].reduce([]) {|matching_people, (label, people)|
+    matching_people << label if people.include? name
     matching_people.uniq
   }
 end
