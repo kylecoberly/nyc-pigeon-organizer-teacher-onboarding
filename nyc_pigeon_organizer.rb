74a6574cf414f06efdb37036dfa2347048fcb810
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-02 00:59:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,6 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # get all people, combine and uniq them
+  data.flat_map {|label, category|
+    category.flat_map {|value, names| names}
+  }
 end
