e975b3ff5c9c603c6573bcfe59461c9fa26aa619
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-02 01:09:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,9 @@ def nyc_pigeon_organizer(data)
   }.uniq
   people.map {|name|
     [name, {
-      :color => find_matches(data, :color)
+      :color => find_matches(data, :color),
+      :gender => find_matches(data, :gender),
+      :lives => find_matches(data, :lives)
     }]
   }.to_h
 end
